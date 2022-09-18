022a47606d35050f33ab5a7e53a34aaa2986ed36
Pablo Alexandre Pamplona <pablompamplona@gmail.com>
2022-09-17 23:23:19 -0300

    H2 database, test profile, JPA

--- src/main/java/com/udemy/course/resources/UserResources.java
+++ src/main/java/com/udemy/course/resources/UserResources.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.udemy.course.entities.User;
+import com.udemy.course.entities.Users;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResources {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99887766", "12345");
+	public ResponseEntity<Users> findAll() {
+		Users u = new Users(1L, "Maria", "maria@gmail.com", "99887766", "12345");
 		return ResponseEntity.ok().body(u);
 		
 	}
