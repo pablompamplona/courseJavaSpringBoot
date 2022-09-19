69907d6306e969bfa15546f547f74ebeccd80ff5
Pablo Alexandre Pamplona <pablompamplona@gmail.com>
2022-09-19 17:01:43 -0300

    Product and ManytoMany assoc with JoinTable

--- src/main/java/com/udemy/course/entities/Category.java
+++ src/main/java/com/udemy/course/entities/Category.java
@@ -1,13 +1,18 @@
 package com.udemy.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -18,6 +23,10 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
@@ -43,6 +52,10 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -60,10 +73,7 @@ public class Category implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	@Override
-	public String toString() {
-		return "Category [id=" + id + ", name=" + name + "]";
-	}
+
 	
 	
 	
